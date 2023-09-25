3628dd2b8d588a980832da3e948acae2e338aaac
volkantabanli <vlkntbnl@gmail.com>
2023-09-25 13:56:30 +0300

    Add Project

---
+++ RabbitMQExample/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var factory = new ConnectionFactory();
+        factory.Uri = new Uri("amqps://zmidlbtr:test@rat.rmq2.cloudamqp.com/test");
+
+        using (var connection = factory.CreateConnection())
+        using (var channel = connection.CreateModel())
+        {
+            string queueName = "example";
+
+            channel.QueueDeclare(queue: queueName,
+                                 durable: true,
+                                 exclusive: false,
+                                 autoDelete: false,
+                                 arguments: null);
+
+            var mesaj = "RabbitMQ test örneğidir.";
+            var body = Encoding.UTF8.GetBytes(mesaj);
+            channel.BasicPublish(String.Empty, queueName, null, body);
+
+            Console.WriteLine("Mesajları almak için bekleniyor. Çıkmak için herhangi bir tuşa basın.");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ReceiveMessage/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var factory = new ConnectionFactory();
+        factory.Uri = new Uri("amqps://zmidlbtr:test@rat.rmq2.cloudamqp.com/test");
+
+        using (var connection = factory.CreateConnection())
+        using (var channel = connection.CreateModel())
+        {
+            string queueName = "example";
+
+            channel.QueueDeclare(queue: queueName,
+                                 durable: true,
+                                 exclusive: false,
+                                 autoDelete: false,
+                                 arguments: null);
+
+            var consumer = new EventingBasicConsumer(channel);
+            channel.BasicConsume(queueName, true, consumer);
+
+            consumer.Received += Consumer_Received;
+
+            Console.ReadLine();
+        }
+    }
+
+    private static void Consumer_Received(object? sender, BasicDeliverEventArgs e)
+    {
+        Console.WriteLine("Gelen Mesaj: " + Encoding.UTF8.GetString(e.Body.ToArray()));
+    }
+}
